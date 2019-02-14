8af64944c5cff2d0b54a7ec82ab4c7484f6d00bd
Aastha <aastha.kumar@sjsu.edu>
2019-02-14 12:47:46 -0800

    updating the stored proc to return first name and last name

--- admin/StudentAdvisingDB/test_stored_prc.sql
+++ admin/StudentAdvisingDB/test_stored_prc.sql
@@ -4,14 +4,12 @@ use advising_admin;
 call prc_user_signup ('Aastha','Kumar','test123','aasthakumar1513@gmail.com','+1','4087805180',true,@result );
 select @result;
 
-insert into OTP values(1, 123456, now(), now()+INTERVAL 15 MINUTE);
-
 -- without OTP
-call prc_user_signin ('aasthakumar1513@gmail.com','test123',false,null,@result );
-select @result;
+call advising_admin.prc_user_signin ('aasthakumar1513@gmail.com','test123',false,null,@result,@f_name, @l_name );
+select @result,@f_name, @l_name;
 
 -- with OTP 
-call prc_user_signin ('aasthakumar1513@gmail.com','',true,123456,@result );
-select @result;
+call prc_user_signin ('aasthakumar1513@gmail.com','',true,123456,@result,@f_name, @l_name );
+select @result,@f_name, @l_name;
 
 -- select now()+INTERVAL 15 MINUTE
