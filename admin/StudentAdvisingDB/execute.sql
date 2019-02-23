e5f66667e28b2b9b6a8bdbad7f25254dcbabb550
Aastha <aastha.kumar@sjsu.edu>
2019-02-23 13:58:55 -0800

    adding the single file to execute all the scripts - follow the instructions in readme to execute that

---
+++ admin/StudentAdvisingDB/execute.sql
@@ -0,0 +1,24 @@
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/1_create_schema.sql
+
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/2_create_table_adminschema.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/3_create_table_advising.sql
+
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/4_insert_statements_qna.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/5_prc_add_category.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/6_prc_add_qna.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/7_prc_get_answer.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/8_prc_get_category_name.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/9_prc_get_question_answer.sql
+
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/10_prc_sign_up.sql
+
+source /Users/aastha/Documents/GitHub/StudentAdvising/admin/StudentAdvisingDB/11_prc_user_signin.sql
