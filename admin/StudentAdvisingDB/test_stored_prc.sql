af40be20f95929de773aed9dd350edd0aa495f05
Aastha <aastha.kumar@sjsu.edu>
2019-04-19 15:51:35 -0700

    updated test script

--- admin/StudentAdvisingDB/test_stored_prc.sql
+++ admin/StudentAdvisingDB/test_stored_prc.sql
@@ -1,13 +1,13 @@
 
-call advising_admin.prc_user_signup ('Aastha','Kumar','test123','aasthakumar1513@gmail.com','+1','4087805180',true,@result );
+call advising_admin.prc_user_signup ('Kanika','Kumar','test123','kk@gmail.com','+1','4087805180',true,@result );
 select @result;
 
 -- without OTP
-call advising_admin.prc_user_signin ('aasthakumar1513@gmail.com','test123',false,null,@result,@f_name, @l_name );
-select @result,@f_name, @l_name;
+call advising_admin.prc_user_signin ('aasthakumar1513@gmail.com','test123',false,null,@result,@f_name, @l_name,@admin );
+select @result,@f_name, @l_name, @admin;
 
 -- with OTP 
-call advising_admin.prc_user_signin ('aasthakumar1513@gmail.com','',true,123456,@result,@f_name, @l_name );
+call advising_admin.prc_user_signin ('aasthakumar1513@gmail.com','',true,123456,@result,@f_name, @l_name,@admin );
 select @result,@f_name, @l_name;
 
 -- select now()+INTERVAL 15 MINUTE
@@ -37,15 +37,17 @@ call advising.prc_add_category('Changing Your Major',@result);
 select @result;
 
 
-call advising.prc_update_qna(1,1, 
-'Permission Codes, or Class Permission Numbers, are required to add a course that indicates Department Consent or Instructor Consent. These codes can be obtained from the appropriate department office or instructor. A permission code is needed after open registration has ended?',
-'Where can I get a permission code?',
+call advising.prc_update_qna(4,4, 
+'You may put yourself on the waitlist during advance registration by registering for the class and checking the waitlist checkbox. If someone drops the course, the first person on the waitlist will be enrolled in the course. (The waitlist cannot add if there is a time conflict, if you are already enrolled in another section of the class, if adding would exceed the unit limit, etc.) However, once advance registration ends, the waitlist will disappear and you will need to attend the first day of class to see if the instructor is able to let you add the course.Login',
+'What if all the classes I want are closed or waitlisted?',
 'Academic Policy',
 'Current Student',@result);
 select @result;
 
 
-call advising.prc_delete_qna(1,1,@result);
+
+
+call advising.prc_delete_qna(9,10,@result);
 select @result;
 
 call advising.prc_get_unanswered_questions();
@@ -54,12 +56,8 @@ call advising.prc_delete_unaswered_q(1,@result);
 select @result;
 
 
-call advising_admin.prc_user_update_signup(2,'disapprove', @result);
+call advising_admin.prc_user_update_signup(8,'approve',1, @result);
 select @result;
 
 call advising_admin.prc_get_all_inactive_users;
-
-
-
-    
 
